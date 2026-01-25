8e1dae075fd3b2341691415da164a439d91a24a7
EverettTucker471 <eftucker@ncsu.edu>
2026-01-25 14:25:50 -0500

    Not correct for D, I don't think ternary search works here

--- Competitions/2026-01-25/d.cpp
+++ Competitions/2026-01-25/d.cpp
@@ -2,6 +2,25 @@
 
 using namespace std;
 
+long long int computeScore(int a[], const vector<int>& m_part, int mid, int n) {
+    // Find out how many swords we can use
+    int swords = 0;
+    for (int i = 0; i < n; i++) {
+        if (a[i] >= mid) swords++;
+    }
+
+    // Find out how many levels we can get to with those swords
+    int level = n;
+    for (int i = 0; i <= n; i++) {
+        if (m_part[i] > swords) {
+            level = i - 1;
+            break;
+        }
+    }
+
+    return (long long int) mid * level;
+}
+
 
 int main() {
     int _;
@@ -21,33 +40,32 @@ int main() {
         }
 
         // Binary search on the difficulty level x
-        long long int rtn = 0;
-        int left = 1;
-        int right = 0;
-        for (auto x : a) right = max(right, x);
+        long long int left = 1;
+        long long int right = 0;
+        for (long long int x : a) right = max(right, x);
 
+        // Computing extrema
+        long long int rtn = max(computeScore(a, m_part, 1, n), computeScore(a, m_part, right, n));
 
         // Should be ternary search not binary?
-        while (left < right) {
-            int mid = (left + right) / 2;
+        while (left <= right) {
+            long long int m1 = (2 * left + right) / 3;
+            long long int m2 = (left + 2 * right + 1) / 3;
+            // cout << "Difficulty: " << m1 << " " << m2 << endl;
 
-            // Find out how many swords we can use
-            int swords = 0;
-            for (auto x : a) {
-                if (x >= mid) swords++;
+            long long int s1 = computeScore(a, m_part, m1, n);
+            long long int s2 = computeScore(a, m_part, m2, n);
+            // cout << "Scores: " << s1 << " " << s2 << endl;
+
+            rtn = max(rtn, max(s1, s2));
+
+            if (s1 > s2) {
+                right = m2 - 1;
+            } else {
+                left = m1 + 1;
             }
-
-            // Find out how many levels we can get to with those swords
-            int level = 0;
-            for (int i = 0; i <= n; i++) {
-                if (m_part[i] > swords) {
-                    level = i - 1;
-                    break;
-                }
-            }
-
-            rtn = max(rtn, (long long int) level * mid);
         }
 
+        cout << rtn << "\n";
     }
 }
